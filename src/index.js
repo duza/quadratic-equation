f9fbac3e918268c5c26e297d137f94bdf95e3cef
duza <duza@tut.by>
2018-09-27 23:57:43 +0300

    Add a solution of quadratic equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let pattern = /(-?\s?\d*\s?)\*\s?x\^2\s?([+-]\s?\d+)\s?\*\s?x\s?([+-]\s?\d+)/;
+  let result = equation.match(pattern).slice(1,4);
+  const [a, b, c] = result.map(num => parseInt(num.replace(/ /gi, ''), 10));
+  const x1 = (-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c))/(2 * a);
+  const x2 = (-b - Math.sqrt(Math.pow(b, 2) - 4 * a * c))/(2 * a);
+  return [x1, x2].map(num => Math.round(num)).sort((a, b) => a - b);
 }
